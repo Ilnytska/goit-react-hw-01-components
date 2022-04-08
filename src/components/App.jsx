a87b7312684eb315ace2e39c5c2dcc2d6187a995
Ilnytska <olly.happyfly@gmail.com>
2022-04-08 20:37:43 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,27 @@
-export const App = () => {
+import { Profile } from './Profile/profile';
+import { Statistics } from './Statistics/statistics';
+import { FriendsList } from './FriendList/friendList';
+import { TransactionHistory } from './TransactionHistory/transactionHistory';
+import user from './Profile/user.json';
+import data from './Statistics/data.json';
+import friends from './FriendList/friends.json';
+import transactions from './TransactionHistory/transaction.json';
+
+
+export function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendsList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+    
+      );
+}
