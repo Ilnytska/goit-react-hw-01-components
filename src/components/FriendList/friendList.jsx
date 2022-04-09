ca5d1d18bca3c1d962ed8094a067a45da47f9cf6
Ilnytska <olly.happyfly@gmail.com>
2022-04-09 23:21:55 +0300

    Update friendList.jsx
    update with FriendListItem component

--- src/components/FriendList/friendList.jsx
+++ src/components/FriendList/friendList.jsx
@@ -1,17 +1,14 @@
 import PropTypes from 'prop-types';
 import styles from './friendList.module.css';
+import { FriendListItem } from 'components/FriendListItem/friendListItem';
 export function FriendsList({ friends }) {
   return (
     <ul className={styles.friendList}>
       {friends.map(({ id, avatar, name, isOnline }) => {
         return (
           <li key={id} className={styles.item}>
-           <span
-        className={styles.isOnline}
-        style={{ backgroundColor: isOnline ? 'green' : 'red' }}
-      ></span>
-            <img className={styles.avatar} src={avatar} alt={name} width="48" />
-            <p className={styles.name}>{name}</p>
+           <FriendListItem avatar={avatar} name={name}  isOnline={isOnline}
+          />
           </li>
         );
       })}
@@ -22,9 +19,6 @@ FriendsList.propTypes = {
   friends: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
-      avatar: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      isOnline: PropTypes.bool.isRequired,
     }),
   ),
 };
