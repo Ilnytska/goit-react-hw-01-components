15c994ce7ee6f640160df7c9df83b6cef7aef969
Ilnytska <olly.happyfly@gmail.com>
2022-04-09 23:19:51 +0300

    Update transactionHistory.jsx
    make destructuring, title prop

--- src/components/Statistics/statistics.jsx
+++ src/components/Statistics/statistics.jsx
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
 import styles from './statistics.module.css';
 export function Statistics({ title, stats }) {
-    return (
-      <section className={styles.statistics}>
-        <h2 className={styles.title}>Upload stats</h2>
+  return (
+    <section className={styles.statistics}>
+      {title !==''  && < h2 className={styles.title}>{title}</h2>}
 
         <ul className={styles.statList}>
-                 {stats.map(stat => (
-                   <li className={styles.item} key={stat.id}>
-                     <span className={styles.label}>{stat.label}</span>
-          <span className={styles.percentage}>{stat.percentage}%</span>
+                 {stats.map(({id, label, percentage})  => (
+                   <li className={styles.item} key={id}>
+                     <span className={styles.label}>{label}</span>
+          <span className={styles.percentage}>{percentage}%</span>
         </li>
       ))}
             </ul>
@@ -17,7 +17,7 @@ export function Statistics({ title, stats }) {
     );
 }
 Statistics.propTypes = {
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
